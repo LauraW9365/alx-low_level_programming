123c7f5eb42101479373130d3e235e8a98e91ea6
LauraW9365 <laura.wafula@gmail.com>
2023-03-16 14:26:02 -0700

    My commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,16 @@
 
 int main(void)
 {
-	printf("Size of a char :%zu byte(s)\n", Sizeof(char));
-	printf("Size of an int :%zu byte(s)\n", Sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char :%lu byte(s)\n", (unsigned long)Sizeof(a));
+	printf("Size of an int :%lu byte(s)\n", Sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
