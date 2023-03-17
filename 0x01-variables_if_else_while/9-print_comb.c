84cecf2b80286fe4c5512e97a1765ba004ec73be
LauraW9365 <laura.wafula@gmail.com>
2023-03-17 13:35:40 -0700

    My commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print all numbers of base 16 in lowercase
+ *  Return: 0 means success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+{
+	putchar(i);
+		if (i != 57)
+	{
+		putchar(44)
+		putchar(32)
+
+	}
+
+	}
+	putchar('\n');
+	return (0);
+}
