a13bfd77ece64d2389745aade1540d5b5d4ab9ba
LauraW9365 <laura.wafula@gmail.com>
2023-03-17 11:08:34 -0700

    My commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print lowcase alphabet
+ * Return: 0 means success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+		putchar(c);
+}
+	putchar('\n');
+
+	for (c = 'A'; c <= 'Z'; c++)
+{
+		putchar(c);
+}
+	putchar('\n');
+	return (0);
+}
