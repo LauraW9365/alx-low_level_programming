38841dd8ebd9167df7bf627b951a0eb96645fd40
LauraW9365 <laura.wafula@gmail.com>
2023-03-21 09:58:24 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - print ten times the alphabet in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int letter = o;
+
+	while (count++ <= 9)
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	_putchar(c);
+
+	_putchar('\n');
+
+}
