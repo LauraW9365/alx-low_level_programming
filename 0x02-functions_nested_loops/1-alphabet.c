855da80bd6b299f7cc96365059f211e2ba0cf341
LauraW9365 <laura.wafula@gmail.com>
2023-03-21 09:03:00 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print lowercase alphabet
+ * Return: 0 means success
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+		_putchar(c);
+}
+	_putchar('\n');
+	return (0);
+}
