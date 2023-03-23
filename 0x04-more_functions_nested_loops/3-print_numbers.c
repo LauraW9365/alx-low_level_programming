7e859d3b69ab9cb5c83f766adf617e17946155ef
LauraW9365 <laura.wafula@gmail.com>
2023-03-23 11:06:59 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - print the numbers from 0 to 9
- *
+ * numbers
  * Return: void
  */
 
@@ -12,8 +12,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-
-		_putchar(c);
+	_putchar(c);
 
 	}
 	_putchar ('\n');
