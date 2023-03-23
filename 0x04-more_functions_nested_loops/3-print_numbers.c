a5ea8c014b127a103c271d1c5d663dfb1fc9ca6d
LauraW9365 <laura.wafula@gmail.com>
2023-03-23 12:26:39 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* print_numbers - print numbers from 0 to 9
-*
-* Return: void
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: void
  */
 
 void print_numbers(void)
