152c8374771766f0e841d10d31ce088b0acef9d6
LauraW9365 <laura.wafula@gmail.com>
2023-03-27 13:24:02 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,12 +2,26 @@
 
 /**
  * rev_string - reverse a string
- * @s - reverse
- *
+ * @s - characater
+ * Description: This will reverse a string
+ * Return: 0
  */
 
 void rev_string(char *s)
-{
-	
 
+{
+int a = 0, b, c;
+	char d;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	c = a - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
 }
