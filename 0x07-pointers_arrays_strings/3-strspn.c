fe24196da46914ea3ed3713e1613352caebd73cf
LauraW9365 <laura.wafula@gmail.com>
2023-04-03 11:49:29 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,10 +3,11 @@
 /**
  * _strspn - gets the length 
  * @s: string
- * @accept: prefix
+ * @accept: prefix to be measured
+ * Return: number of bytes
 */
 
 unsigned int _strspn(char *s, char *accept)
 {
-
+ 
 }
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches and finds any bytes in the string accept
+ * @s: string to be searched
+ * @accept: bytes to be looked for
+ * Return: a pointer or null
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int q;
+
+	while (*s)
+	{
+		for (q = 0; accept[q]; q++)
+		{
+			if (*s == accept[q])
+				return (s);
+		}
+		 s++;
+	}
+	return ('\0');
+}
