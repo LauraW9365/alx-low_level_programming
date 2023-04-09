cea2b731809d19e7ee70d7aa2ec766bef063e332
LauraW9365 <laura.wafula@gmail.com>
2023-04-09 07:06:20 -0700

    1-args.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints it's own program name folowed by a new line
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 always
+*/
+
+int main(int arc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed
+ * @argc: count of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 always
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
