72c7bd73e805eb0918e0b336c35faae9c84e8031
LauraW9365 <laura.wafula@gmail.com>
2023-04-17 10:03:57 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the file it was compiled from
+ *
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
