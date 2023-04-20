d19fc64c24c572d65ad7004b91d8df2171606897
LauraW9365 <laura.wafula@gmail.com>
2023-04-20 11:41:28 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,7 +4,7 @@
 /**
  * print_strings - prints strings followed by a new line
  * @separator: string to be printed between the strings
- * @n: number of strings passed to teh function
+ * @n: number of strings passed to the function
 */
 
 void print_strings(const char *separator, const unsigned int n, ...)
